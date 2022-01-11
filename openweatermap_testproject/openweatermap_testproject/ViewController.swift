908e45b0bfa369eb438f43d1dbf2927fc053dd6b
fedir72 <fedir72@ua.fm>
2022-01-11 11:13:13 +0200

    empty project

---
+++ openweatermap_testproject/openweatermap_testproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  openweatermap_testproject
+//
+//  Created by Fedii Ihor on 11.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
